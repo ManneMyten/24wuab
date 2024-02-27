66ed786474ce32d11251bab8796f8c71f36bdaea
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-02-27 14:16:32 +0100

    Skapat en bättre burger-meny samt mer responsiv

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let menubutton = document.querySelector(".menu-button")
+let menu = document.querySelector(".menu")
+let fade_layer = document.querySelector(".fade-layer")
+
+function show(){
+    menu.classList.toggle("show")
+    fade_layer.classList.toggle("visible")
+}
+
+fade_layer.addEventListener("click",show)
+menubutton.addEventListener("click",show)
