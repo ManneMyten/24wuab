961e914c8185935ee24d80e348b6e3d631ba2d8c
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-02-29 13:39:12 +0100

    carousel kinda works

--- js/main.js
+++ js/main.js
@@ -10,7 +10,14 @@ function show(){
 fade_layer.addEventListener("click",show)
 menubutton.addEventListener("click",show)
 
+let slide1 = document.querySelector("#slide1")
+let slide2 = document.querySelector("#slide2")
+let slide3 = document.querySelector("#slide3")
+
 let slideIndex = 1
 function slideMove(n) {
     slideIndex += n
+    if (slideIndex == 1){
+
+    }
 }
