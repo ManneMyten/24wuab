544437726c9f70ff6c4afe38a363fb4ee78fb07f
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-03-02 17:44:15 +0100

    Börjat på card sida

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 let menubutton = document.querySelector(".menu-button")
 let menu = document.querySelector(".menu")
 let fade_layer = document.querySelector(".fade-layer")
+let header = document.querySelector("header")
 
 function show(){
     menu.classList.toggle("show")
     fade_layer.classList.toggle("visible")
+    // header.classList.toggle("visible")
 }
 
 fade_layer.addEventListener("click",show)
 menubutton.addEventListener("click",show)
-
-let slide1 = document.querySelector("#slide1")
-let slide2 = document.querySelector("#slide2")
-let slide3 = document.querySelector("#slide3")
-
-let slideIndex = 1
-function slideMove(n) {
-    slideIndex += n
-    if (slideIndex == 1){
-
-    }
-}
